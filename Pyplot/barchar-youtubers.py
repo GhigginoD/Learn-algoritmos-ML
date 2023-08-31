61f9819e81e435d486df1b0fb33c7c5c304040fe
Davi <davighiggino_otg@cnc.org.br>
2023-08-31 20:27:38 -0300

    implementando criação de gráficos dos resultados de uma planilha

---
+++ Pyplot/barchar-youtubers.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+base_de_dados = pd.read_csv("../data/channel-youtubers.csv", delimiter=",", encoding="ISO-8859-1")
+
+top_10_highest_subscribed = base_de_dados[["Youtuber", "subscribers", "category"]].head(
+    10
+)
+
+plt.barh(
+    top_10_highest_subscribed["Youtuber"],
+    top_10_highest_subscribed["subscribers"],
+    label="Number of Subscribers",
+)
+plt.xlabel("Subscribers in million")
+plt.ylabel("Top_10_Youtubers")
+
+plt.legend()
+plt.show()
